c365576154ee048a00b9c056ca9f78b5979bce96
P.J. Janse van Rensburg <petrus.jvrensburg@gmail.com>
2022-08-08 12:07:12 +0200

    fix typo & broken link

--- lib/cldr/trans/translator.ex
+++ lib/cldr/trans/translator.ex
@@ -2,7 +2,7 @@ defmodule Cldr.Trans.Translator do
   @moduledoc """
   Provides easy access to struct translations.
 
-  Although translations are stored in regular fields of an struct and can be accessed directly, **it
+  Although translations are stored in regular fields of a struct and can be accessed directly, **it
   is recommended to access translations using the functions provided by this module** instead. This
   functions present additional behaviours such as:
 
@@ -15,7 +15,7 @@ defmodule Cldr.Trans.Translator do
   * Translating entire structs.
 
   All examples in this module assume the following article, based on the schema defined in
-  [Structured translations](Trans.html#module-structured-translations)
+  [Structured translations](Cldr.Trans.html#module-structured-translations)
 
       article = %MyApp.Article{
         title: "How to Write a Spelling Corrector",
