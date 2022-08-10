87579fb491e8bd4c77700edfe7b3af25e1b8f0c8
P.J. Janse van Rensburg <petrus.jvrensburg@gmail.com>
2022-08-10 08:33:38 +0200

    test: configure default locale in the same place as supported locales.

--- test/support/myapp_cldr_backend.ex
+++ test/support/myapp_cldr_backend.ex
@@ -2,6 +2,7 @@ defmodule MyApp.Cldr do
   use Cldr,
     gettext: MyApp.Gettext,
     locales: ["en", "de", "ja", "en-AU", "th", "ar", "pl", "doi", "fr-CA", "nb", "no"],
+    default_locale: "en-001",
     generate_docs: true,
     providers: [Cldr.Trans]
 end
